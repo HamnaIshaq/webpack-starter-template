3ef10bffafda06573ff43774a012e593a8b65268
Hamna Ishaq <hamnaishaq1@gmail.com>
2023-04-22 00:47:22 +0500

    Add HTML content using JS module

--- src/index.js
+++ src/index.js
@@ -1,1 +1,11 @@
 import "./style.css";
+
+function component() {
+  const element = document.createElement("div");
+
+  element.textContent = "Hello World";
+
+  return element;
+}
+
+document.body.appendChild(component());
